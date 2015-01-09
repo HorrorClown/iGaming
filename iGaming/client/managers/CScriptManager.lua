65159c8c977e7e1ff73df994510c795d47650b0c
Yannick Z <YannickZiermann@gmx.de>
2015-01-09 11:11:37 +0100

    Create CScriptManager.lua

---
+++ iGaming/client/managers/CScriptManager.lua
@@ -0,0 +1,11 @@
+-- 
+-- HorrorClown (PewX)
+-- Using: IntelliJ IDEA 14 Ultimate
+-- Date: 09.01.2014 - Time: 11:11
+-- pewx.de // iGaming-mta.de // iRace-mta.de // iSurvival.de // mtasa.de
+--
+CScriptManager = {}
+
+function CScriptManager:constructor()
+
+end
